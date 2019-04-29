dacc78165a94e97d9f8e23a296acd20b89905dcd
kenanaga <kenanaga1020@gmail.com>
2019-04-30 01:44:15 +0400

    committ

---
+++ bdb.php
@@ -0,0 +1,11 @@
+<?php
+$db= new PDO ("mysql:host=localhost;dbname=blog;charset=utf8","root","");
+  
+  $query = $db->query("SELECT * FROM haqqimizda", PDO::FETCH_ASSOC);
+if ( $query->rowCount() ){
+     foreach( $query as $row ){
+          
+     }
+}
+
+?>
